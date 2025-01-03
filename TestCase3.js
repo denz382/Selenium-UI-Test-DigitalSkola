455c2855f089dabd1ad00773c8c8a2203081e531
deny nurhadi <deny.nurhadi56@gmail.com>
2025-01-03 15:35:05 +0700

    Page Object Model Task

--- TestCase3.js
+++ TestCase3.js
@@ -6,40 +6,72 @@ const assert = require(`assert`);
 const fs = require(`fs`);
 
 const screenshotDir = `./screenshots/`;
+if(!fs.existsSync(screenshotDir)) {const { Builder } = require (`selenium-webdriver`);
+const CartPage = require (`../webComponent/CartPage`);
+const assert = require(`assert`);
+const fs = require(`fs`);
+require(`dotenv`).config();
+
+const browser = process.env.BROWSER;
+
+switch(browser.toLowerCase()) {
+    case `firefox` :
+        const firefox = require(`selenium-webdriver/firefox`);
+        options = new firefox.Options();
+        options.addArguments(`--headless`);
+
+    case `edge` :
+        const edge = require(`selenium-webdriver/edge`);
+        options = new edge.Options();
+       
+    case `chrome` :
+    default :
+        const chrome = require(`selenium-webdriver/chrome`);
+        options = new chrome.Options();
+        options.addArguments(`--headless`);
+        break;
+}
+
+const screenshotDir = `./screenshots/`;
 if(!fs.existsSync(screenshotDir)) {
     fs.mkdirSync(screenshotDir, {recursive: true});
 }
 
-describe(`TestCase 3`, function() {
+describe(`TestCase 3 #Regression`, function() {
     this.timeout(40000);
     let driver;
 
     // Run setiap mulai test, satu kali saja paling awal
     before(async function() {
-        driver = await new Builder().forBrowser(`chrome`).build();
+        driver = await new Builder().forBrowser(browser).setChromeOptions(options).build();
     });
 
     //Test Suite di mulai dengan apa, setiap melakukan tes
     beforeEach(async function () {
-        const loginPage = new LoginPage(driver);
         const cartPage = new CartPage(driver);
-        await loginPage.navigate();
-        await loginPage.login(`standard_user`, `secret_sauce`);
+        await cartPage.navigate('https://www.saucedemo.com/inventory.html');
+        await cartPage.addCart();
         
     });
 
     //Assertion atau validasi 
-    it(`Login Successfuly and verify dashboard`, async function () {
-        const dashboardPage = new DashboardPage(driver);
-        const title = await dashboardPage.isOnDashboard();
-        assert.strictEqual(title, `Products`, `Expected dashboard title to be Products`);
-    });
-   
+  
     it(`Add Item To Cart Succesfuly`, async function () {
         const cartPage = new CartPage(driver);
-        const title = await cartPage.isOnCart();
-        assert.strictEqual(title, `Products`, `Description Item`);
+        const badgeText = await cartPage.isonDashboard();
+        assert.strictEqual(badgeText, `1`, `Expected cart badge to show "1"`);
     });
+
+    afterEach(async function () {
+        const screenshot = await driver.takeScreenshot();
+        const filepath = `${screenshotDir}${this.currentTest.title.replace(/\s+/g,`_`)}_${Date.now()}.png`
+        fs.writeFileSync (filepath, screenshot, `base64`);
+    });
+
+    after(async function() {
+        await driver.quit();
+    });
+});
 
 
 
